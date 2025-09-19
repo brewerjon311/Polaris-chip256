91c694e4188d0dca39f90a181bfca15b764bfdcf
brewerjon311 <brewerjonathan662@gmail.com>
2025-09-19 11:30:18 -0400

    Update my-card.js

--- src/my-card.js
+++ src/my-card.js
@@ -22,11 +22,117 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+
+.card.fancy {
+  background-color: black;
+}
+h1 {
+  font-size: 32px;
+}
+
+h3,h4,h5,h6 {
+  margin: 8px 0;
+}
+
+#card-list {
+  display: flex;
+}
+.card {
+  font-size: 16px;
+  display: inline-flex;
+  border: 8px blue;
+  padding: 8px;
+  margin: 8px;
+  background-color: green;
+  transition: .6s all ease-in-out;
+}
+
+.card-image {
+  width: 200px;
+  height: 100%;
+}
+
+.card-text {
+  width: 350px;
+  padding: 0 8px 8px 8px;
+  color: black;
+  background-color: white;
+  margin: 0 0 0 8px;
+  overflow: auto;
+}
+
+.card-title {
+  position: sticky;
+  top: 0;
+  background-color: #eeeeee;
+  text-align: center;
+  font-size: 2em;
+  padding: 8px 8px 16px;
+  margin: 16px;
+}
+
+ul {
+
+  padding: 16px;
+}
+
+ul 
+li {
+  padding: 8px 16px;
+  list-style: square;
+}
+
+ul 
+li:hover {
+  font-weight: bold;
+}
+
+.links li:focus-within,
+.links li:hover {
+  background-color: white;
+  color: blue;
+}
+.links li a:focus,
+.links li:hover a {
+  color: green;
+  text-decoration: underline;
+  cursor: move;
+  outline: none;
+}
+
+.card:hover,
+.card:focus-within {
+  opacity: 1;
+  outline: 2px solid green;
+  outline-offset: 16px;
+}
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`<div>${this.title}</div>
+    <h1>Mount Nittany</h1>
+<div id="cardlist">
+  <div class="card">
+    <img class="card-image" alt="https://pabucketlist.com/wp-content/uploads/2020/07/Boalsburg-Overlook-Mount-Nittany.jpg" />
+    <div class="card-text">
+      <h3 class="card-title">Mount Nittany</h3>
+      <div class="card-details">
+        <p>
+        This is the mountain where Penn State University Park is located. The word Nittany is Algonquian in origin and is part of the Appalacian mountain range. The mountain is famous for its hiking trainls and stunning views.
+        </p>
+        <h4>Facts</h4>
+        <ul>
+          <li>In PA</li>
+          <li>Is tall</li>
+          <li>Third fact go here</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+    
+    `;
   }
 
   static get properties() {
